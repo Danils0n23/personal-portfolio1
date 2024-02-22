eaaea40638852bb0507e1054bbaa6eb4b4584608
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-02-22 06:16:12 +0100

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Image from '../assets/avatar.svg'
+
+const Banner = () => {
+  return <div className='section' id='home'>
+    Banner
+  </div>;
+};
+
+export default Banner;
