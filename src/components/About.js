91d91c95281f32827984c35f1fbd8e7a05d3e36d
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-02-23 23:42:03 +0100

    Merge changes from main into develop

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const About = () => {
-  return <div className='section' id='about'>
-    About
-  </div>;
+  const [ref, InView] = useInView({
+    threshold: 0.5,
+  });
+
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten b-top'>
+          </motion.div>
+
+          <motion.div className='flex-1'
+            variants={fadeIn('left', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}>
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4'>
+              I am a technology and software development enthusiast,
+              an avid basketball player, and a lover of photography.
+            </h3>
+            <p className='mb-6'>Passionate about tech, software, basketball, and urban photography, I've found my calling in frontend development. Combining tech and art, I create captivating digital experiences inspired by city life.</p>
+
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient'>
+                  {InView ? <CountUp start={0} end={10} duration={3} /> : null}
+                  k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Projects <br />
+                  completed
+                </div>
+              </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient'>
+                  {InView ? <CountUp start={0} end={9} duration={3} /> : null}
+                  k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Satisfied <br />
+                  Clients
+                </div>
+              </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient'>
+                  {InView ? <CountUp start={0} end={5} duration={3} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  experiences
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href='https://github.com/Danils0n23/' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,48 +1,103 @@
 import React from 'react';
-
-import Image from '../assets/avatar1.png'
-
-import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
-
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
-
 import { motion } from 'framer-motion';
-
-import { fadeIn } from '../variants'
+import { fadeIn } from '../variants';
+import Image from '../assets/avatar1.png';
 
 const Banner = () => {
-  return <section className='section' id='home'>
-    <div>
-      <div>
-        <div>
-          <h1>
-            DANILSON <spa>BENGUI Jr 23</spa>
-          </h1>
-          <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
-            <span className='text-white mr-4'>I am a </span>
-            <TypeAnimation
-              sequence={[
-                'Software Developer',
-                2000,
-                'Baskettball Player',
-                2000,
-                'Photographer',
-                2000,
-              ]}
-              speed={50}
-              className='text-accent'
-              wrapper='span'
-              repeat={Infinity}
-            />
+  return (
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+              DANILSON <span>BENGUI Jr 23</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+              <span className='text-white mr-4'>I am a </span>
+              <TypeAnimation
+                sequence={[
+                  'Software Developer',
+                  2000,
+                  'Baskettball Player',
+                  2000,
+                  'Photographer',
+                  2000,
+                ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>
+              Hello! I'm passionate about basketball, software development, and photography. <br /> Basketball taught me competitiveness and teamwork, skills I apply in software development.
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href='https://github.com/Danils0n23/Solola-App' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </motion.div>
+
+            <motion.div
+              variants={fadeIn('up', 0.7)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+
+              <a href='https://www.instagram.com/denilson_davidson23/'>
+                <FaInstagram />
+              </a>
+
+              <a href='https://github.com/Danils0n23'>
+                <FaGithub />
+              </a>
+
+              <a href='https://www.linkedin.com/in/danilson-j%C3%BAnior-bengui-501226260/'>
+                <FaLinkedin />
+              </a>
+
+              <a href='https://wa.me/927981648'>
+                <FaWhatsapp />
+              </a>
+
+            </motion.div>
           </div>
-          <p> Hello! I'm passionate about basketball, software development, and photography. <br /> Basketball taught me competitiveness and teamwork, skills I apply in software development.</p>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+            <img src={Image} alt="" />
+          </motion.div>
         </div>
+
       </div>
-      <div>
-        <img src={Image} alt="" />
-      </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default Banner;
