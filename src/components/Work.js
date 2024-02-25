0bfb7cd57e7082d3e30e723b95f7249f26902c7c
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-02-25 15:37:30 +0100

    fix servies

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,21 +8,21 @@ import img3 from '../assets/portfolio-img3.png';
 const Work = () => {
   return (
     <section className='section' id='work'>
-      <div className='conteiner mx-auto'>
+      <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
           <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
-            <div>
+            <div className='flex-1 flex-col gap-y-10'>
               <h2 className='h2 leading-tight text-accent'>
                 My Latest <br />
                 Work.
               </h2>
-              <p className='max-w-sm mb-16'>
+              <p className='max-w-sm mb-6'>
                 Represents a significant addition to my portfolio, showcasing my skills in frontend and backend development, as well as my understanding of real-time communication technologies.
               </p>
               <button className='btn btn-sm'>View all projects</button>
             </div>
 
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
 
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
 
@@ -36,8 +36,39 @@ const Work = () => {
                 <span className='tex-3x1 text-white'>Portfolio</span>
               </div>
             </div>
+
           </div>
-          <div className='flex-1'>2</div>
+          <div className='flex-1 flex flex-col gap-y-10'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+
+              <img className='group-hover:scale-125 transition-all duration-500 z-50' src={img1} alt='' />
+
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Software Developer</span>
+              </div>
+
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='tex-3x1 text-white'>Portfolio</span>
+              </div>
+            </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+
+              <img className='group-hover:scale-125 transition-all duration-500 z-50' src={img1} alt='' />
+
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Software Developer</span>
+              </div>
+
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='tex-3x1 text-white'>Portfolio</span>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
